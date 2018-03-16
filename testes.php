059291f9a44fc8db93b71616c72becef73065fae
Jose Paulo <pauloneto.jose@gmail.com>
2018-03-16 03:20:59 -0300

    Delete testes.php

--- testes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$arrayz = [1,2,3,4,5,6,7,8,9,10];
-$pageSize = 3;
-$pageNumber = 0;
-echo "\npagina numero {$pageNumber}\n";
-$arrays = array_slice($arrayz,$pageNumber*$pageSize , $pageSize);
-var_dump($arrays);
